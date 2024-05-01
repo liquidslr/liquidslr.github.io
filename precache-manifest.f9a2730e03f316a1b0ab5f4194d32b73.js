a1fb33a2fb77219db8efdc68685345bd303e2ec6
Gaurav Kumar <gaurav.kumar9825@gmail.com>
2024-04-30 21:45:53 -0400

    Updates

---
+++ precache-manifest.f9a2730e03f316a1b0ab5f4194d32b73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc4e4eae0bb3c604a20ec67bf6c5f2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51bad0db46e3b0d35cd2",
+    "url": "/static/css/main.7642b214.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "51bad0db46e3b0d35cd2",
+    "url": "/static/js/main.be81198c.chunk.js"
+  },
+  {
+    "revision": "60bc7f864dddbc003152",
+    "url": "/static/js/runtime~main.71a348c3.js"
+  }
+]);
